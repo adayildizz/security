62b67488320247e88a9b208c0431977a326962e5
adayildizz <adayildizz51@gmail.com>
2025-10-07 14:41:04 +0200

    report added.

--- assignment1/code/client/main.go
+++ assignment1/code/client/main.go
@@ -31,10 +31,9 @@ func main() {
 		log.Fatalf("failed to append CA cert: %s", *caPath)
 	}
 
-	// 2) Build TLS creds (verify serverName against cert)
+	
 	creds := credentials.NewClientTLSFromCert(cp, *serverName)
 
-	// 3) Dial with TLS
 	conn, err := grpc.Dial(
 		*addr,
 		grpc.WithTransportCredentials(creds),
@@ -45,7 +44,7 @@ func main() {
 	}
 	defer conn.Close()
 
-	// 4) Call RPC
+	
 	client := pb.NewMessengerClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
